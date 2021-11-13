75720250635d437b5e61503bb7a60b9b222e3c8a
Inigo Surguy <inigo.surguy@gmail.com>
2021-11-13 19:14:39 +0000

    Moving keyframe generation into ScalaJS

--- js/src/main/scala/net/surguy/dice/Game.scala
+++ js/src/main/scala/net/surguy/dice/Game.scala
@@ -1,5 +1,6 @@
 package net.surguy.dice
 
+import org.scalajs.dom.CSSStyleSheet
 import org.scalajs.dom.html.Canvas
 
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
@@ -9,9 +10,44 @@ import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
   */
 @JSExportTopLevel("dice")
 object Game {
+
+  import org.scalajs.dom._
+
   @JSExport
   def play(canvas: Canvas): Unit = {
+    val sheet = newStylesheet()
 
+    val keyframes = Keyframes("bounce", Seq(
+      Keyframe(0, "transform: translate3d(0, 0, 0)"),
+      Keyframe(20, "transform: translate3d(100px, 500px, 20px)"),
+      Keyframe(40, "transform: translate3d(200px, 100px, 40px)"),
+      Keyframe(60, "transform: translate3d(300px, 500px, 60px)"),
+      Keyframe(80, "transform: translate3d(400px, 200px, 80px)"),
+      Keyframe(100, "transform: translate3d(500px, 500px, 100px)"),
+    ))
+
+    keyframes.addToSheet(sheet)
   }
 
+  def addKeyframes(frames: Seq[Keyframes]): CSSStyleSheet = {
+    val sheet = newStylesheet()
+    frames.foreach(f => sheet.insertRule(f.toCss))
+    sheet
+  }
+
+  def newStylesheet(): CSSStyleSheet = {
+    val stylesheetEl = document.createElement("style").asInstanceOf[HTMLStyleElement]
+    document.head.appendChild(stylesheetEl)
+    stylesheetEl.sheet.asInstanceOf[CSSStyleSheet]
+  }
+
+}
+
+case class Keyframe(pct: Int, rule: String) {
+  def toCss: String = s""" $pct% { $rule }"""
+}
+
+case class Keyframes(name: String, frames: Seq[Keyframe]) {
+  def toCss: String = s"""@keyframes $name {\n ${frames.map(_.toCss).mkString("\n") } }\n\n"""
+  def addToSheet(sheet: CSSStyleSheet): Unit = sheet.insertRule(toCss)
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,3 +5,7 @@ addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.7.1")
 addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.6.0")
 
 addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject" % "1.0.0")
+
+// Workaround for "scalajsCom is not defined" bug
+// https://github.com/scala-js/scala-js-js-envs/issues/12
+libraryDependencies += "org.scala-js" %% "scalajs-env-nodejs" % "1.2.1"
