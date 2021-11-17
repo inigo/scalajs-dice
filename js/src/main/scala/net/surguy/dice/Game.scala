4f9ce0e9a769f2e4e954acc295ebe3a9afa96220
Inigo Surguy <inigo.surguy@gmail.com>
2021-11-17 20:25:42 +0000

    Move die creation and motion into ScalaJS

--- js/src/main/scala/net/surguy/dice/Game.scala
+++ js/src/main/scala/net/surguy/dice/Game.scala
@@ -1,7 +1,9 @@
 package net.surguy.dice
 
 import net.surguy.dice.css.{Keyframes, TranslateKeyframe}
-import org.scalajs.dom.html.Canvas
+import net.surguy.dice.shared.Die
+import org.scalajs.dom.html.{Canvas, Div}
+import scalatags.JsDom
 
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
@@ -12,30 +14,43 @@ import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 object Game {
 
   import org.scalajs.dom._
+  import scalatags.JsDom.all._
 
   @JSExport
   def play(canvas: Canvas): Unit = {
     val sheet = newStylesheet()
+    val (id1, id2) = ("one", "two")
 
-    val keyframes = Keyframes("bounce", Seq(
-      TranslateKeyframe(0,0,0),
-      TranslateKeyframe(100,500,20),
-      TranslateKeyframe(200,100,40),
-      TranslateKeyframe(300,500,60),
-      TranslateKeyframe(400,200,80),
-      TranslateKeyframe(500,500,100),
-     ))
-    sheet.insertRule(keyframes.toCss)
+    val keyframes1 = Keyframes(s"die-animation-$id1",
+      (0 to 5).map(i => TranslateKeyframe(i * 120,if (i%2!=0) 500 else i * 50,i * 20))
+    )
+    sheet.insertRule(keyframes1.toCss)
 
-    //    val textarea = document.createElement("textarea" ).asInstanceOf[HTMLTextAreaElement]
-//    textarea.value = keyframes.toCss
-//    document.body.appendChild(textarea)
+    val keyframes2 = Keyframes(s"die-animation-$id2",
+      (0 to 7).map(i => TranslateKeyframe(i * 120,if (i%2!=0) 500 else i * 20,i * -50))
+    )
+    sheet.insertRule(keyframes2.toCss)
+
+//    document.body.appendChild(textarea(keyframes2.toCss).render)
+
+    val dice = div(cls := "dice", displayDie(6, "red", id1), displayDie(6, "purple", id2))
+    document.body.appendChild(dice.render)
   }
 
   def newStylesheet(): CSSStyleSheet = {
     val stylesheetEl = document.createElement("style").asInstanceOf[HTMLStyleElement]
     document.head.appendChild(stylesheetEl)
     stylesheetEl.sheet.asInstanceOf[CSSStyleSheet]
+  }
+
+  private def displayDie(die: Die): JsDom.TypedTag[Div] = displayDie(die.sides, die.appearance.color, die.id.getOrElse("noid"))
+
+  private def displayDie(sides: Int, color: String, id: String): JsDom.TypedTag[Div] = {
+    div(cls := s"die d$sides die-$color", style := s"animation-name: die-animation-$id",
+      div(cls := "die-faces",
+        (1 to sides).map(d => div(d.toString))
+      )
+    )
   }
 
 }
